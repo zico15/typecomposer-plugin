0de1f1cd6af8cd6a951e42c068fb9edbfd380f33
Ezequiel Carlos <zequielzico@gmail.com>
2024-01-05 17:46:27 +0000

    check

--- src/plugin/base/ProjectBuild.ts
+++ src/plugin/base/ProjectBuild.ts
@@ -41,8 +41,9 @@ export class ProjectBuild extends Project {
 
     constructor() {
         super();
-        this.addSourceFilesAtPaths('**/*.ts');
+        this.addSourceFilesAtPaths('node_modules/typecompose-plugin/**/*.ts');
         this.path = this.getSourceFiles().find(e => e.getFilePath().includes("node_modules/typecompose-plugin"))?.getFilePath() || "";
+
         if (this.path != "")
             this.path = this.path.split("node_modules/typecompose-plugin/")[0] + "node_modules/typecompose-plugin/";
         this.stylePath = this.path + "public/style.scss";
